ef6cde8142f6fe274fede98bb22c7d80cc9483fb
lesf25 <dmitry.leschev@improvado.io>
2026-02-19 12:23:47 +0700

    Fix inheritance SQLAlchemyRespository

--- backend/src/app/infrastructure/repositories/base.py
+++ backend/src/app/infrastructure/repositories/base.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.app.domain.repositories.base_repository import AbstractRepository
 
-class SQLAlchemyAbstractRepository:
+
+class SQLAlchemyAbstractRepository(AbstractRepository):
     def __init__(self, session: AsyncSession) -> None:
         self._session = session
